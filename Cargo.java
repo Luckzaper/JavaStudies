17779dd9339118a5e6a9502bcea6b07591b26046
Luckas Olimpio <117861573+Luckzaper@users.noreply.github.com>
2025-02-26 23:41:55 -0500

    First idea of a truck management system

---
+++ Cargo.java
@@ -0,0 +1,26 @@
+public class Cargo {
+
+      String description;
+
+    //constructor
+    public Cargo(String description){
+        this.description = description;
+    }
+
+    //setter and getter
+
+    public void setCargo(String description){
+        this.description = description;
+    }
+
+    public String getCargo(){
+        return this.description;
+    }
+
+
+
+
+
+
+
+}
